afdd57d3e5cbc693658459085d2ac2160fd733ee
MALLADISARMA <143084170+MALLADISARMA@users.noreply.github.com>
2025-06-09 20:22:33 +0530

    orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express"
 import authMiddleware from "../middleware/auth.js"
-import { listOrders, placeOrder, updateStatus, userbookings,  verifyOrder } from "../controllers/orderController.js"
+import { deleteOrder, listOrders, placeOrder, updateStatus, userbookings,  verifyOrder } from "../controllers/orderController.js"
 
 const orderRouter=express.Router();
 orderRouter.post("/place",authMiddleware,placeOrder);
@@ -8,5 +8,7 @@ orderRouter.post("/verify",verifyOrder);
 orderRouter.post("/userbookings",authMiddleware,userbookings);
 orderRouter.get("/list",listOrders)
 orderRouter.post("/status",updateStatus)
+orderRouter.post("/delete",deleteOrder); // No auth needed for admin
+
 
 export default orderRouter;
